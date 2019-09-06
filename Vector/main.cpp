506fe732e13314b4c20ffd3ab676a8e114870b18
killedmyself <markmisra@gmail.com>
2019-09-06 14:26:23 +0300

    Add specification Minor refactoring

--- Vector/main.cpp
+++ Vector/main.cpp
@@ -1,7 +1,21 @@
 #include <iostream>
 #include <vector>
 
+//- добавление элемента после последнего;
+//-добавление элемента перед первым; 
+//-добавление элемента по порядку;
+//-удаление элемента с указанной информационной частью;
+//-поиск элемента;
+//-удаление всех элементов;
+//-сортировка элементов;
+//-упорядочение текущего элемента(предполагается, что все остальные элементы упорядочены);
+//-перегруженный оператор !, определяющий существование элементов в структуре данных;
 
+//-копирование структуры данных с помощью перегруженного оператора присваивания;
+//-перегруженный оператор[] при написании класса для работы с массивом;
+//-получение количества элементов в массиве;
+//-возможность пользователя изменять количество зарезервированной под новые элементы памяти;
+//-автоматическое определение количества зарезервированной под новые элементы памяти на основании размера одного элемента.
 
 template <typename T>
 class dynamic_array
@@ -14,40 +28,36 @@ private:
 	bool MemAllocation();
 
 public:
+	dynamic_array();
+	dynamic_array(const size_t Size);
 	dynamic_array(const dynamic_array& Obj);
-
-	dynamic_array& operator=(dynamic_array& Obj);
+	dynamic_array& operator=(const dynamic_array& Obj);
+	~dynamic_array();
 
 	T& operator[](const size_t Index);
+	bool operator!();
 
 	size_t size() const noexcept;
 	void resize(const size_t Size);
 	void reserve(const size_t Size);
 
-	void push_back(const T& Value);
-	void push_front(const T& Value);
-	void emplace(const T& Value, size_t Position);
+	void push_back(const T Value);
+	void push_front(const T Value);
+	void emplace(const T Value, size_t Position);
 	
-	void erase(const T& Value);
+	void erase(const T Value);
+	void clean();
 
-	T& search(const T& Value) const;
+	T& search(const T Value) const;
 
 	void sort();
+	void order_element(const T value);
 
-	bool operator!();
 };
-
-template<typename T>
-bool dynamic_array<T>::MemAllocation()
-{
-	m_data = (T*)realloc(m_data, m_size);
-	
-}
 
 int main()
 {
-	
-	std::vector<int> as;
+
 
 	system("pause");
 	return 0;
